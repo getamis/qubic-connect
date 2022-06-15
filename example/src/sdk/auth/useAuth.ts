ebf284f3bb9f2b82cbb6b0e2abc60de24ea4ae54
ROXYCHEN <work.rxc714168@gmail.com>
2022-06-15 15:41:47 +0800

    fix sigg

--- example/src/sdk/auth/useAuth.ts
+++ example/src/sdk/auth/useAuth.ts
@@ -3,23 +3,49 @@ import Web3 from 'web3';
 import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
 import { Web3Provider, ExternalProvider } from '@ethersproject/providers';
 import { AbstractProvider } from 'web3-core';
+import { QubicConnector } from '@qubic-js/react';
 
 import { useQubicAuthConnector } from './useQubicAuthConnector';
-// import serviceHeaderBuilder from '../utils/serviceHeaderBuilder';
 import convertStringToHex from '../utils/convertStringToHex';
+
+import {
+  // API_KEY,
+  // API_SECRET,
+  // CREATOR_API,
+  QUBIC_API_KEY,
+  QUBIC_API_SECRET,
+  QUBIC_CHAIN_ID,
+  QUBIC_INFURA_PROJECT_ID,
+} from '../constants/environment';
+
+interface CustomizeProvider extends ExternalProvider {
+  isQubic?: boolean;
+}
+
+let qubicConnector: QubicConnector;
 
 const useAuth = () => {
   const context = useWeb3React<Web3Provider>();
   const { account, activate, deactivate, library: ethersProvider } = context;
-  const { qubicConnector, isAlignWithServerTime } = useQubicAuthConnector();
   const [startSign, setStartSign] = useState(false);
-  const [loginMsg, setLoginMsg] = useState('');
 
   const [authSignature, setAuthSignature] = useState<string | undefined>('');
   const [signDataString, setSignDataString] = useState<string>('');
   const [isQubicUser, setIsQubicUser] = useState(true);
 
-  // const hasUserData = !!user?.expiredAt;
+  const currentProvider = ethersProvider?.provider as CustomizeProvider;
+
+  useEffect(() => {
+    qubicConnector = new QubicConnector({
+      apiKey: QUBIC_API_KEY,
+      apiSecret: QUBIC_API_SECRET,
+      chainId: Number(QUBIC_CHAIN_ID),
+      infuraProjectId: QUBIC_INFURA_PROJECT_ID,
+      autoHideWelcome: true,
+      enableIframe: true,
+    });
+  }, []);
+
   const handleSignAuthData = useCallback(
     async (payload: any) => {
       if (!account || !payload) {
@@ -27,61 +53,60 @@ const useAuth = () => {
       }
 
       try {
-        const currentProvider = window.web3?.currentProvider as AbstractProvider;
         const signature = currentProvider?.request ? await currentProvider.request(payload) : '';
-
         // When user reject or close qubic-wallet, will get Error as return value
         if (typeof signature !== 'string') {
           throw signature;
         }
         setAuthSignature(signature);
+        console.log(signature);
       } catch (error) {
         console.error(error);
-        setLoginMsg('簽章失敗');
+        console.error('簽章失敗');
       }
     },
-    [account],
+    [account, currentProvider],
   );
 
   const handleLogin = useCallback(
     async (ev?: any) => {
+      if (typeof window === 'undefined') return;
       ev?.preventDefault();
-      setLoginMsg('');
-      console.log(qubicConnector);
-      if (qubicConnector && typeof window !== 'undefined') {
-        console.log('sign');
+      try {
         await activate(qubicConnector, (e: Error): void => {
-          console.error(e);
-          setLoginMsg('Login to Qubic fail.');
+          throw e;
         });
         setStartSign(true);
+      } catch (err) {
+        console.error(err);
+        console.error('Login to Qubic fail.');
       }
     },
-    [activate, qubicConnector],
+    [activate],
   );
 
-  // useEffect(() => {
-  //   if (account && startSign && ethersProvider) {
-  //     const currentProvider = ethersProvider.provider as QubicProvider;
-  //     const dataString = JSON.stringify({
-  //       name: AUTH_APP_NAME,
-  //       url: `https://${AUTH_APP_DOMAIN}/`,
-  //       permissions: ['wallet.permission.access_email_address'],
-  //       nonce: Date.now(),
-  //       service: AUTH_SERVICE_NAME,
-  //     });
-  //     setSignDataString(dataString);
-  //     const isQubic = !!currentProvider?.isQubic;
-  //     setIsQubicUser(isQubic);
-  //     const payload = {
-  //       jsonrpc: '2.0',
-  //       method: isQubic ? 'qubic_login' : 'personal_sign',
-  //       params: [account, isQubic ? dataString : convertStringToHex(dataString)],
-  //     };
-  //     handleSignAuthData(payload);
-  //     setStartSign(false);
-  //   }
-  // }, [account, ethersProvider]);
+  useEffect(() => {
+    const isQubic = !!currentProvider?.isQubic;
+
+    if (account && startSign && currentProvider && isQubic) {
+      const dataString = JSON.stringify({
+        name: 'OneOffs',
+        url: 'https://nft.oneoffs.art',
+        permissions: ['wallet.permission.access_email_address'],
+        nonce: Date.now(),
+        service: 'qubee-creator',
+      });
+      setSignDataString(dataString);
+      setIsQubicUser(isQubic);
+      const payload = {
+        jsonrpc: '2.0',
+        method: isQubic ? 'qubic_login' : 'personal_sign',
+        params: [account, isQubic ? dataString : convertStringToHex(dataString)],
+      };
+      handleSignAuthData(payload);
+      setStartSign(false);
+    }
+  }, [account, currentProvider, currentProvider?.isQubic, ethersProvider, handleSignAuthData, startSign]);
 
   return {
     // user,
