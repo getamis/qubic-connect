e313361b1d7b7ea8502350e5f68be8aa70ae5e85
Steven Fong <waneblade@gmail.com>
2023-05-25 11:26:02 +0800

    [ASL-3961] bind with redirect 5 * updated example

--- examples/example-pure-js/src/index.ts
+++ examples/example-pure-js/src/index.ts
@@ -159,11 +159,7 @@ function main() {
         memberId,
       });
       if (!success) throw Error('success failed');
-
-      const member = await loginClientServer({ memberId });
-
-      const user = await qubicConnect.loginWithCredential(member.credential);
-      window.alert('user login in:' + JSON.stringify(user));
+      window.alert(`success bind, no you can login with memberId ${memberId}`);
     } catch (error) {
       if (error instanceof Error) {
         window.alert(error.message);
@@ -195,12 +191,6 @@ function main() {
       },
     });
     window.alert(JSON.stringify(ETHToTWDCurrencyData));
-  });
-
-  const bindPrimeDom = document.querySelector('#bind-prime');
-  bindPrimeDom?.addEventListener('click', async () => {
-    const response = await qubicConnect.bindWithRedirect();
-    window.alert(JSON.stringify(response));
   });
 
   async function buyAsset(locale?: PaymentLocale) {
@@ -344,12 +334,18 @@ function main() {
     qubicConnect.bindWithRedirect();
   });
 
-  document.getElementById('login-with-credential')?.addEventListener('click', () => {
-    const credential = prompt('credential in json format');
-    if (!credential) {
-      throw Error('null credential');
+  document.getElementById('bind-mock-login')?.addEventListener('click', async () => {
+    const memberId = window.prompt('what memberId you want to login');
+    try {
+      if (!memberId) throw Error('no memberId');
+      const member = await loginClientServer({ memberId });
+      const user = await qubicConnect.loginWithCredential(member.credential);
+      window.alert('user login in:' + JSON.stringify(user));
+    } catch (error) {
+      if (error instanceof Error) {
+        window.alert(error.message);
+      }
     }
-    qubicConnect.loginWithCredential(JSON.parse(credential));
   });
 
   document.getElementById('redirect-login-metamask')?.addEventListener('click', () => {
